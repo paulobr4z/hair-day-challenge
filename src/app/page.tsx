50cbf1e5678438e4c144d0eeee62ef5f25d40b6c
paulobr4z <paulobrazaraujo@gmail.com>
2026-01-13 15:51:17 -0300

    feat: config color e icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-gray-800">
       <h1>Home</h1>
     </div>
   );
